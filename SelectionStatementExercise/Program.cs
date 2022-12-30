5413dda55167aad1e27461fc75e7af1ae7937ea6
ross dibartolo <dibartoloross@gmail.com>
2022-12-30 12:15:02 -0500

    Second commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -8,23 +8,23 @@
             var favNumber = r.Next(1, 1000);
             Console.WriteLine("What is my favorite number?");
             var userInput = int.Parse(Console.ReadLine());
-            
-                if (userInput <= 88)
-            {        
-                Console.WriteLine("too low");
-            }
-            
-                else if (userInput >= 90)
-            {        
-                Console.WriteLine("too high");
-            }
-           
-                else
-            { 
-                Console.WriteLine("You guessed it!!");
-            }
-            
 
+            //    if (userInput < favNumber)
+            //{        
+            //    Console.WriteLine("too low");
+            //}
+
+            //    else if (userInput > favNumber)
+            //{        
+            //    Console.WriteLine("too high");
+            //}
+
+            //    else
+            //{ 
+            //    Console.WriteLine("You guessed it!!");
+            //}
+
+            Console.WriteLine((userInput < favNumber) ? "too low" : (userInput > favNumber) ? "too high" : "You guessed it!!");
         }
     }
 }
